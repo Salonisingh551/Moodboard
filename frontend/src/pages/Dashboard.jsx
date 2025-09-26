acb56af949c45d0dab95b89a60931f0ef84941e0
Saloni Singh <salonisngh615@gmail.com>
2025-09-27 01:08:17 +0530

    Update Dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -87,22 +87,22 @@ export default function Dashboard() {
     }
   }
 
-  function handleLogout(){
-    localStorage.removeItem("token");
-    navigate("/login");
-  }
+  // function handleLogout(){
+  //   localStorage.removeItem("token");
+  //   navigate("/login");
+  // }
 
 
   return(
     <div className="p-6 max-w-3xl mx-auto">
       <h1 className="text-2xl font-bold">Dashboard</h1>
 
-      <button 
+      {/* <button 
       onClick={handleLogout} 
       className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
 >
       Logout
-     </button>
+     </button> */}
 
       <form onSubmit={(e)=>{
         e.preventDefault();
